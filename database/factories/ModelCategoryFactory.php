da900fdf72e4d4be6000d06f2fb37a4c761735fc
bigfielddigital <bigfield@cutes-MacBook-Pro.local>
2018-04-20 20:39:53 +0100

    factory and seed

--- database/factories/ModelCategoryFactory.php
+++ database/factories/ModelCategoryFactory.php
@@ -3,7 +3,11 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Category::class, function (Faker $faker) {
+
+    $word = $faker->sentence;
+
     return [
-        //
+        'name' => $word,
+        'slug' => str_slug($word)
     ];
 });
